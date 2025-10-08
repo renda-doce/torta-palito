85fbf261c478d72407f5370fc4e59dd2860a175d
Roberto Tavares <roberto.tavares@trt2.jus.br>
2025-10-08 11:42:54 -0300

    Adiciona eventos

--- js/links-whatsapp.js
+++ js/links-whatsapp.js
@@ -8,16 +8,40 @@ const link_direto = 'https://wa.me/5511951190867?text=Ol%C3%A1!%20%0AN%C3%A3o%20
 
 // Atualiza todos os links dinamicamente para os botões de grupo
 document.querySelectorAll('.link-group').forEach(a => {
-    a.setAttribute("href", link_grupo);
-    a.setAttribute("target", "_blank"); // abre em nova aba
-    a.setAttribute("rel", "noopener noreferrer"); //
+  // Atualiza os links dinamicamente
+  a.setAttribute("href", link_grupo);
+  a.setAttribute("target", "_blank");
+  a.setAttribute("rel", "noopener noreferrer");
+
+  // Listener do evento
+  a.addEventListener("click", function () {
+    // Evita duplicação de evento
+    if (!this.dataset.pixelSent) {
+
+      // Monta descrição segura e padronizada
+      const botao_id = this.id ? this.id : 'sem-id';
+
+      fbq('track', 'Lead', {
+        content_name: `Lead - Torta Palito`,
+        content_id: botao_id,
+        value: 2.00,
+        currency: 'BRL',
+        event_source: window.location.origin,
+        page_path: window.location.pathname,
+      });
+
+      this.dataset.pixelSent = "true";
+      console.log("✅ Evento Meta Pixel 'Lead' enviado com sucesso.");
+    }
+  });
 });
+
 
 // Atualiza todos os links dinamicamente para os botões de contato
 document.querySelectorAll('.link-direto').forEach(a => {
-    a.setAttribute("href", link_direto);
-    a.setAttribute("target", "_blank"); // abre em nova aba
-    a.setAttribute("rel", "noopener noreferrer"); //
+  a.setAttribute("href", link_direto);
+  a.setAttribute("target", "_blank"); // abre em nova aba
+  a.setAttribute("rel", "noopener noreferrer"); //
 });
 
 
